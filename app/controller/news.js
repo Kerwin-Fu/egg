0ba3fd333643f0aca329d2ec61bf68be0f6862fc
KerwinFu <1078933746@qq.com>
2022-09-01 20:52:26 +0800

    build: 创建了我的eggdemo

---
+++ app/controller/news.js
@@ -0,0 +1,14 @@
+const Controller = require('egg').Controller
+
+class NewsController extends Controller {
+  async list() {
+    const { ctx } = this
+    ctx.body = {
+      id: '11',
+      username: 'zs',
+      age: '18'
+    }
+  }
+}
+
+module.exports = NewsController
